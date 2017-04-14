e6d9af8dfd7d252f5009aad5575d5f27f293de8b
militoj <militoj@gmail.com>
2017-04-14 00:12:43 -0500

    added add form handler in MenuController

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,11 +1,14 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Cheese;
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by johnmilito on 4/13/17.
@@ -28,5 +31,14 @@ public class MenuController {
         model.addAttribute("title", "My Menus");
 
         return "menu/index";
+    }
+
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddMenuForm(Model model) {
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+        model.addAttribute("categories", menuDao.findAll());
+        return "menu/add";
     }
 }
