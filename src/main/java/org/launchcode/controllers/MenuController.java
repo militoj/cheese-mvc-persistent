0a39f223a6872f0784ecaff196a4e2dc02285d2d
militoj <militoj@gmail.com>
2017-04-14 00:29:35 -0500

    added post handler to MenuController

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.Cheese;
 import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
@@ -7,8 +8,12 @@ import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by johnmilito on 4/13/17.
@@ -40,5 +45,19 @@ public class MenuController {
         model.addAttribute(new Menu());
         model.addAttribute("categories", menuDao.findAll());
         return "menu/add";
+    }
+
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddMenuForm(Model model, @ModelAttribute @Valid Menu menu,
+                                         Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+        }
+
+        menuDao.save(menu);
+        return "redirect:view/" + menu.getId();
     }
 }
